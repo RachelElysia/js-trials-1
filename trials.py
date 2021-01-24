d742368c4659d19cafc9693c6cfc883ec0ae4e39
Rachel Elysia Perkins <rachel.elysia.perkins@gmail.com>
2021-01-24 15:54:52 +0000

    Finish 4 of 10 Functions

--- trials.py
+++ trials.py
@@ -2,23 +2,31 @@
 
 
 def output_all_items(items):
-    pass  # TODO: replace this line with your code
-
+    for item in items:
+        print(item)
 
 def get_all_evens(nums):
-    pass  # TODO: replace this line with your code
+    evenNums = []
+    for num in nums:
+        if num % 2 == 0:
+            evenNums.append(num)
+    
+    return evenNums
 
 
 def get_odd_indices(items):
-    pass  # TODO: replace this line with your code
+    result = []
 
+    for i in range(len(items)):
+        if i%2 ==0:
+            result.append(items[i])
 
 def print_as_numbered_list(items):
-    pass  # TODO: replace this line with your code
-
+    for count, value in enumerate(items):
+        print (count, value)
 
 def get_range(start, stop):
-    pass  # TODO: replace this line with your code
+    
 
 
 def censor_vowels(word):
